55a492fb147ead02199ea9123369ad2074e67e21
BuddhiGayan2000 <108883603+BuddhiGayan2000@users.noreply.github.com>
2024-04-11 11:04:23 +0530

    admin functions removed

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./src/routes/userRoutes');
 
 
-const artCategoryRouter = require('./src/routes/art-categories.routes');
-const userRouter = require('./src/routes/artist-request.routes');
-const dashboardRouter = require('./src/routes/dashboard.router');
-const userManagementRouter = require('./src/routes/user-management.routes');
 const artistPageRouter = require('./src/routes/artist-page.routes');
 const forYouRouter = require('./src/routes/foryou.routes');
 const cartRouter = require('./src/routes/cart.routes');
@@ -43,11 +39,6 @@ app.use((req, res, next) => {
 
 
 app.use('/user', userRoutes);
-
-app.use('/art-categories', artCategoryRouter);
-app.use('/artist-request', userRouter);
-app.use('/dashboard', dashboardRouter);
-app.use('/user-management', userManagementRouter);
 
 app.use('/artist-page', artistPageRouter);
 app.use('/for-you', forYouRouter);
