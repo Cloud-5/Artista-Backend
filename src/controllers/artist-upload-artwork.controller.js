ecfc2fd64b951cd0123fcf3480890b3a9ba32ae1
Dhanushkarangana <karunarathnarmdrr.21@uom.lk>
2024-07-10 12:32:07 +0530

    ....

--- src/controllers/artist-upload-artwork.controller.js
+++ src/controllers/artist-upload-artwork.controller.js
@@ -1,5 +1,5 @@
-//const { uploadFileArtCategoriesToS3, getArtwork, deleteArtwork } = require('../services/artworkService');
-const {ArtCategories,ArtCategoriesFormats} = require('../services/artist-upload-artwork.service');
+
+const {ArtCategories,ArtCategoriesFormats, UploadArtwork, ArtworkTags,  ArtworkTools, ArtworkFileFormats} = require('../services/artist-upload-artwork.service');
 
 // exports.uploadArtwork = async (req, res, next) => {
 //   try {
@@ -65,3 +65,59 @@ const {ArtCategories,ArtCategoriesFormats} = require('../services/artist-upload-
         next(error);
     }
   };
+
+exports.upload2DArtwork = async (req, res, next) => {
+  const { artist,title,price,thumbnail_url,description,category_id,is3D, tag_name, tools, fileFormats } = req.body;
+  try {
+      console.log('artist',artist);
+    const result2D = await UploadArtwork.upload2DArt(artist,title,price,thumbnail_url,description,category_id,is3D);
+
+    const artworkId = result2D.artworkId;
+
+    const tagsPromises = tag_name.map(tag => {
+        return ArtworkTags.addTags(artworkId,tag.tag_name);
+    });
+    const toolsPromises = tools.map(tool => {
+        return ArtworkTools.addTool(artworkId, tool.tool_name);
+    });
+  
+    const fileFormatsPromises = fileFormats.map(format => {
+        return ArtworkFileFormats.addFileFormat(artworkId, format.file_format_name);
+    });
+
+
+    await Promise.all([...tagsPromises, ...toolsPromises, ...fileFormatsPromises]);
+    //await Promise.all(tagsPromises);
+    res.status(201).json({message: '2D Artwork uploaded successfully', artworkId});
+  } catch (error) {
+      console.error('Error uploading 2D artwork');
+      next(error);
+  }
+}
+
+exports.upload3DArtwork = async (req, res, next) => {
+    const { artist,title,price,thumbnail_url,description,category_id,is3D, subfolder_name, modelBackground, original_url, tag_name, tools, fileFormats } = req.body;
+    try {
+        const result3D = await UploadArtwork.upload3DArt(artist,title,price,thumbnail_url,description,category_id,is3D,subfolder_name, modelBackground,original_url);
+  
+        const artworkId = result3D.artworkId;
+  
+        const tagsPromises = tag_name.map(tag => {
+            return ArtworkTags.addTags(artworkId,tag.tag_name);
+        });
+        const toolsPromises = tools.map(tool => {
+            return ArtworkTools.addTool(artworkId, tool.tool_name);
+        });
+      
+        const fileFormatsPromises = fileFormats.map(format => {
+            return ArtworkFileFormats.addFileFormat(artworkId, format.file_format_name);
+        });
+  
+  
+        await Promise.all([...tagsPromises, ...toolsPromises, ...fileFormatsPromises]);
+        res.status(201).json({message: '3D Artwork uploaded successfully', artworkId});
+    } catch (error) {
+        console.error('Error uploading 3D artwork');
+        next(error);
+    }
+}
