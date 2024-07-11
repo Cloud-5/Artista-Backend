1231925bb28ee62b8f49ca8abe5c435bd494f0b9
Gihan Shyavinda Jayathissa <jayathissakdgs.21@uom.lk>
2024-07-11 15:55:41 +0530

    ...

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -244,6 +244,6 @@ exports.changePassword = async (req, res) => {
       return res.status(200).json({ message: "Password changed successfully" });
   } catch (error) {
       console.error('Error changing password:', error);
-      return res.status(500).json({ message: "Internal server error" });
+      return res.status(500).json({ message: "Internal server error"});
 }
 };
