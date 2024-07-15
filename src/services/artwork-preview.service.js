90025f15a51d4a78f6b3097c5f269ec1f48eac2e
BuddhiGayan2000 <108883603+BuddhiGayan2000@users.noreply.github.com>
2024-07-15 15:52:55 +0530

    ...

--- src/services/artwork-preview.service.js
+++ src/services/artwork-preview.service.js
@@ -4,73 +4,77 @@ class ArtPreview {
   static getArtDetails(artId, userId) {
     return db.execute(
       `SELECT 
-        a.title AS artwork_name,
-        CONCAT(u.fName, ' ', u.LName) AS artist,
-        u.profession AS profession,
-        u.location AS location,
-        u.user_id AS artist_id,
-        c.name AS category,
-        a.price AS price,
-        a.published_date AS published_date,
-        COALESCE(al.total_likes, 0) AS total_likes,
-        a.description AS description,
-        GROUP_CONCAT(DISTINCT ato.tool_name) AS tools,
-        GROUP_CONCAT(DISTINCT af.file_format_name) AS formats,
-        GROUP_CONCAT(DISTINCT atg.tag_name) AS tags,
-        a.original_url AS url_link,
-        a.modelBackground As background,
-        a.thumbnail_url AS thumbnail,
-        COUNT(DISTINCT a2.artwork_id) AS total_creations,
-        AVG(ar.rating_value) AS avg_rating,
-        COUNT(DISTINCT afollower.follower_user_id) AS followers_count,
-        u.description AS artist_description,
-        u.profile_photo_url AS artist_profile_photo,
-        COUNT(DISTINCT com.comment_id) AS total_comments,
-        CASE 
-            WHEN a_like.user_id IS NOT NULL THEN 1 
-            ELSE 0 
-        END AS is_liked,
-        CASE 
-            WHEN afollower.followed_artist_user_id IS NOT NULL THEN 1 
-            ELSE 0 
-        END AS is_following,
-        CASE 
-            WHEN g.artwork_id IS NOT NULL THEN 1 
-            ELSE 0 
-        END AS is_addedToGallery
-    FROM 
-        artwork a
-    JOIN 
-        user u ON a.artist_id = u.user_id
-    JOIN 
-        category c ON a.category_id = c.category_id
-    LEFT JOIN 
-        (SELECT artwork_id, COUNT(*) AS total_likes FROM artwork_like GROUP BY artwork_id) al 
-        ON a.artwork_id = al.artwork_id
-    LEFT JOIN 
-        artwork_tool ato ON a.artwork_id = ato.artwork_id
-    LEFT JOIN 
-        artwork_file_format af ON a.artwork_id = af.artwork_id
-    LEFT JOIN 
-        artwork_tag atg ON a.artwork_id = atg.artwork_id
-    LEFT JOIN 
-        artwork a2 ON a.artist_id = a2.artist_id
-    LEFT JOIN 
-        artist_rating ar ON u.user_id = ar.rated_user_id
-    LEFT JOIN 
-        artist_follower afollower ON u.user_id = afollower.followed_artist_user_id AND afollower.follower_user_id = ?
-    LEFT JOIN
-        comment com ON a.artwork_id = com.artwork_id
-    LEFT JOIN
-        artwork_like a_like ON a.artwork_id = a_like.artwork_id AND a_like.user_id = ?
-    LEFT JOIN
-        gallery g ON a.artwork_id = g.artwork_id AND g.customer_user_id = ?
-    WHERE 
-        a.artwork_id = ?
-    GROUP BY 
-        a.artwork_id;
+    a.title AS artwork_name,
+    CONCAT(u.fName, ' ', u.LName) AS artist,
+    u.profession AS profession,
+    u.location AS location,
+    u.user_id AS artist_id,
+    c.name AS category,
+    a.price AS price,
+    a.published_date AS published_date,
+    COALESCE(al.total_likes, 0) AS total_likes,
+    a.description AS description,
+    GROUP_CONCAT(DISTINCT ato.tool_name) AS tools,
+    GROUP_CONCAT(DISTINCT af.file_format_name) AS formats,
+    GROUP_CONCAT(DISTINCT atg.tag_name) AS tags,
+    a.original_url AS url_link,
+    a.modelBackground AS background,
+    a.thumbnail_url AS thumbnail,
+    COUNT(DISTINCT a2.artwork_id) AS total_creations,
+    COALESCE(AVG(ar.rating_value), 0) AS avg_rating,
+    COUNT(DISTINCT afollower.follower_user_id) AS followers_count,
+    u.description AS artist_description,
+    u.profile_photo_url AS artist_profile_photo,
+    COUNT(DISTINCT com.comment_id) AS total_comments,
+    CASE 
+        WHEN a_like.user_id IS NOT NULL THEN 1 
+        ELSE 0 
+    END AS is_liked,
+    CASE 
+        WHEN afollower.followed_artist_user_id IS NOT NULL THEN 1 
+        ELSE 0 
+    END AS is_following,
+    CASE 
+        WHEN g.artwork_id IS NOT NULL THEN 1 
+        ELSE 0 
+    END AS is_addedToGallery,
+    cust.profile_photo_url AS customer_profile_photo
+FROM 
+    artwork a
+JOIN 
+    user u ON a.artist_id = u.user_id
+JOIN 
+    category c ON a.category_id = c.category_id
+LEFT JOIN 
+    (SELECT artwork_id, COUNT(*) AS total_likes FROM artwork_like GROUP BY artwork_id) al 
+    ON a.artwork_id = al.artwork_id
+LEFT JOIN 
+    artwork_tool ato ON a.artwork_id = ato.artwork_id
+LEFT JOIN 
+    artwork_file_format af ON a.artwork_id = af.artwork_id
+LEFT JOIN 
+    artwork_tag atg ON a.artwork_id = atg.artwork_id
+LEFT JOIN 
+    artwork a2 ON a.artist_id = a2.artist_id
+LEFT JOIN 
+    artist_rating ar ON u.user_id = ar.rated_user_id
+LEFT JOIN 
+    artist_follower afollower ON u.user_id = afollower.followed_artist_user_id AND afollower.follower_user_id = ?
+LEFT JOIN
+    comment com ON a.artwork_id = com.artwork_id
+LEFT JOIN
+    artwork_like a_like ON a.artwork_id = a_like.artwork_id AND a_like.user_id = ?
+LEFT JOIN
+    gallery g ON a.artwork_id = g.artwork_id AND g.customer_user_id = ?
+LEFT JOIN
+    user cust ON cust.user_id = ?
+WHERE 
+    a.artwork_id = ?
+GROUP BY 
+    a.artwork_id;
+
     `,
-      [userId, userId, userId, artId]
+      [userId, userId, userId, userId, artId]
     );
   }
 
@@ -107,8 +111,9 @@ class ArtPreview {
     );
   }
 
-  static getRelatedArtworks(artId){
-    return db.execute("CALL GetRelatedArtworks(?);",[artId]);
+  static getRelatedArtworks(artId,userId) {
+    console.log(artId,userId);
+    return db.execute("CALL GetRelatedArtworks(?,?);", [artId,userId]);
   }
 
   static getComments(artId) {
@@ -188,7 +193,7 @@ class ArtPreview {
 
   static InsertLike(artId, userId) {
     return db.execute(
-      "INSERT INTO artwork_like (artwork_id, user_id) VALUES (?, ?)",
+      "INSERT INTO artwork_like (artwork_id, user_id, liked_at) VALUES (?, ?,  NOW())",
       [artId, userId]
     );
   }
@@ -202,7 +207,7 @@ class ArtPreview {
 
   static InsertFollow(customerId, artistId) {
     return db.execute(
-      "INSERT INTO artist_follower (follower_user_id, followed_artist_user_id) VALUES (?, ?)",
+      "INSERT INTO artist_follower (follower_user_id, followed_artist_user_id, follow_date) VALUES (?, ?, NOW())",
       [customerId, artistId]
     );
   }
