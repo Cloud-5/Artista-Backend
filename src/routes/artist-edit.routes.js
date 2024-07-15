026eaf2fd0bea82c626c8793dee5efa3135c6806
Dhanushkarangana <karunarathnarmdrr.21@uom.lk>
2024-07-15 15:44:45 +0530

    bbb

--- src/routes/artist-edit.routes.js
+++ src/routes/artist-edit.routes.js
@@ -4,6 +4,13 @@ const artistEdit = require('../controllers/artist-edit.controller');
 const Artist = require('../models/artist-page.model')
 
 // Route to update artist details
-// router.put('/:artistId',artistEdit.updateArtist);
+router.put('/:artistId',artistEdit.updateArtist);
 
+
+
+
+// router.post('/:artistId/social-media', artistEdit.addSocialMediaLink);
+router.put('/:artistId/social-media', artistEdit.updateSocialMediaLink);
+// router.get('/:userId', artistEdit.getFollowers);
+router.get('/social-media-platforms',artistEdit.getSocialMediaPlatforms);
 module.exports= router;
